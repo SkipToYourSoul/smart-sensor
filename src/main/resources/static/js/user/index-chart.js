1672d63ff6e6383d782eb25b4ff66ce9c088620c
SkipToYourSoul <ly.forwork@foxmail.com>
2017-07-28 01:36:46 +0800

    modify on 2017-07-28

---
+++ src/main/resources/static/js/user/index-chart.js
@@ -0,0 +1,28 @@
+/**
+ * Belongs to
+ * Author: liye on 2017/7/28
+ * Description:
+ */
+
+var myChart = echarts.init(document.getElementById('chart'));
+
+var option = {
+    title: {
+        text: 'ECharts 入门示例'
+    },
+    tooltip: {},
+    legend: {
+        data:['销量']
+    },
+    xAxis: {
+        data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
+    },
+    yAxis: {},
+    series: [{
+        name: '销量',
+        type: 'bar',
+        data: [5, 20, 36, 10, 10, 20]
+    }]
+};
+
+myChart.setOption(option);
